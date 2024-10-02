c2fa4f1aae14e78cf3b69d6b457d193894d01878
AbdulAHad365 <amir.raza537918@gmail.com>
2024-10-02 11:16:13 -0400

    Initial commant

---
+++ set_timeout.js
@@ -0,0 +1,7 @@
+// #Blocking methods execute synchronously and 
+// non-blocking methods execute asynchronously.
+
+// This is the blocking methord
+
+setTimeout(()=>console.log("TURTLE WINS"),1000);
+console.log("RABBIT WINS")
