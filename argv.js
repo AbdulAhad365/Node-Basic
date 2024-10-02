d41d471a3493960b81dc55ff1bb43d58ddd56a62
AbdulAHad365 <amir.raza537918@gmail.com>
2024-10-02 10:59:15 -0400

    Add argv.js

---
+++ argv.js
@@ -0,0 +1,9 @@
+const store=process.argv[2]
+// const store="ahad";
+// console.log("hello")
+if (store==="ahad"){
+    console.log("welcome to node")
+}
+else{
+    console.log(`are u sure ${store} to continue`)
+}
